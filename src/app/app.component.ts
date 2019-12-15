0d9ea2644a2b797456eefbec3bcc008a5314212d
Naoufal ALAOUI <alaouiismailinaoufal@gmail.com>
2019-12-15 14:24:43 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Team } from './team';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tutorial';
+  teams = Team;
+
 }
